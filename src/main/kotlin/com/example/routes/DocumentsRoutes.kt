b3e37d0ea9ecb35c6c75964fbf6b436379b24578
João Garrido <joao.melo.garrido@gmail.com>
2023-04-02 13:41:21 +0100

    file uploading changed query a bit

--- src/main/kotlin/com/example/routes/DocumentsRoutes.kt
+++ src/main/kotlin/com/example/routes/DocumentsRoutes.kt
@@ -1,6 +1,7 @@
 package com.example.routes
 
 import com.example.models.Patient
+import com.example.models.ServerResponse
 import com.example.services.DocumentsServices
 import io.ktor.http.content.*
 import io.ktor.server.application.*
@@ -9,34 +10,31 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import java.io.File
+import java.io.IOException
 import java.util.*
 
-fun Route.documentsRoutes(documentsServices : DocumentsServices) {
-
+fun Route.documentsRoutes(documentsServices: DocumentsServices) {
     authenticate("patient-interaction") {
         post("documents/upload") {
             val patient = call.principal<Patient>()?.patient_id
-            val multipartData = call.receiveMultipart()
-            var fileDescription = ""
-            var fileName = ""
-            multipartData.forEachPart { part ->
-                when (part) {
-                    is PartData.FormItem -> {
-                        fileDescription = part.value
-                    }
-
-                    is PartData.FileItem -> {
-                        val name = part.originalFileName as String
-                        fileName = "${UUID.randomUUID()}-$name"
+            lateinit var fileName: String
+            try {
+                val multipartData = call.receiveMultipart()
+                multipartData.forEachPart { part ->
+                    if (part is PartData.FileItem) {
+                        val name = part.originalFileName.toString()
+                        fileName = "$patient-${UUID.randomUUID()}-$name"
                         val fileBytes = part.streamProvider().readBytes()
                         File("uploads/$fileName").writeBytes(fileBytes)
                     }
-
-                    else -> {}
+                    part.dispose()
                 }
-                part.dispose()
+                call.respond(ServerResponse(true, "File Uploaded"))
+            } catch (e: Exception) {
+                call.respond(ServerResponse(false, "Unable to upload file"))
             }
-            call.respondText("$fileDescription is uploaded to 'uploads/$fileName'")
+
+
         }
     }
 }
